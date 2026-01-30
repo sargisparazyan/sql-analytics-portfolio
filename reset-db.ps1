a4ce1aeab799176ca0544b27aea1a596af2accf0
sargisparazyan <saqparazyan5518@gmail.com>
2026-01-30 20:14:16 +0400

    adding new reset-db.ps1 file

---
+++ reset-db.ps1
@@ -0,0 +1,8 @@
+$ErrorActionPreference = "Stop"  # Exit immediately if any command fails
+Write-Host "Stopping containers..."
+docker compose down
+Write-Host "Removing Postgres data directory..."
+Remove-Item -Recurse -Force .\postgres_data -ErrorAction SilentlyContinue
+Write-Host "Starting containers..."
+docker compose up
+Write-Host "Database reset complete. Init scripts reran successfully."
